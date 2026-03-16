8e253facb7f08388f5cca9b46c31f10b67ce0251
gabtoledano <gabrieltoledano19@gmail.com>
2026-03-16 13:40:32 +0100

    fix: events - application du filtre par catégorie et calcul pageNumber corrigé

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -13,11 +13,13 @@ const EventList = () => {
   const { data, error } = useData();
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
-  const filteredEvents = (
+
+  const filteredByType =
     (!type
       ? data?.events
-      : data?.events) || []
-  ).filter((event, index) => {
+      : data?.events.filter((event) => event.type === type)) || [];
+
+  const filteredEvents = filteredByType.filter((event, index) => {
     if (
       (currentPage - 1) * PER_PAGE <= index &&
       PER_PAGE * currentPage > index
@@ -26,12 +28,15 @@ const EventList = () => {
     }
     return false;
   });
+
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
   };
-  const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
+
+  const pageNumber = Math.floor((filteredByType?.length || 0) / PER_PAGE) + 1;
   const typeList = new Set(data?.events.map((event) => event.type));
+
   return (
     <>
       {error && <div>An error occured</div>}
